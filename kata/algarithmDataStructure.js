f7bbc8a8c3a9aceb4c3a0b7394a7e4f1439a422b
rohan-fa <rohanfarzana@gmail..com>
2023-09-12 12:49:39 +1000

    Big o notation intro

---
+++ kata/algarithmDataStructure.js
@@ -0,0 +1,5 @@
+//Big O Notation gives us a precise, numeric and objective war of judging the performance of our code. Just by reading it
+// why does Big O Notation / Time Complexity Matter? 
+
+//it's helps you write better code and becomes important as your inputs get bigger(EG Users/ Posts for an app)
+
