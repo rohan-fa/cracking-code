e06f31f7b08ef83bfdc080c2555a43d70b728b4d
rohan-fa <rohanfarzana@gmail..com>
2023-09-12 20:13:17 +1000

    commenting on the testing stacks

--- kata/algarithmDataStructure.js
+++ kata/algarithmDataStructure.js
@@ -3,3 +3,8 @@
 
 //it's helps you write better code and becomes important as your inputs get bigger(EG Users/ Posts for an app)
 
+//testing dependencies 
+// - mocha.js javascript test framework for node.js and the javascript on the browser
+//chai and sinon are two other js files that make it easier to write are the testing code for mocha and make it a bit more dynamic.
+
+
