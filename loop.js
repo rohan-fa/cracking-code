76654f47b91a1f05e1ad8b9883b0321515803dda
rohan-fa <rohanfarzana@gmail..com>
2023-10-14 16:39:00 +1100

    maps, filter, chaining

--- loop.js
+++ loop.js
@@ -175,6 +175,56 @@ const myNums = [1,2,3,2,1,3,42,13,4]
 
 
 
+// ~~~~~~~~~~~~~~~~ BOOKS ~~~~~~~~~~~~~~~~~~~~~~
+
+const books = [
+    { title: 'Book One', genre: 'Fiction', publish: 1981, edition: 2004 },
+    { title: 'Book Two', genre: 'Non-Fiction', publish: 1992, edition: 2008 },
+    { title: 'Book Three', genre: 'History', publish: 1999, edition: 2007 },
+    { title: 'Book Four', genre: 'Non-Fiction', publish: 1989, edition: 2010 },
+    { title: 'Book Five', genre: 'Science', publish: 2009, edition: 2014 },
+    { title: 'Book Six', genre: 'Fiction', publish: 1987, edition: 2010 },
+    { title: 'Book Seven', genre: 'History', publish: 1986, edition: 1996 },
+    { title: 'Book Eight', genre: 'Science', publish: 2011, edition: 2016 },
+    { title: 'Book Nine', genre: 'Non-Fiction', publish: 1981, edition: 1989 },
+  ];
+
+let userBooks = books.filter((bk) => bk.genre === 'History');
+userBooks = books.filter((bk) => {
+    return bk.publish >= 1995 && bk.genre === "History"})
+
+
+//console.log(userBooks);
+
+// ~~~~~~~~~~~~~~~~~~~  MAP ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+const myNumbs = [1,43,22,4,3,2,4,25,3,4,46,7,64,5]
+
+//const nun = myNumbs.map((num) => num + 10 )
+
+//console.log(nun);
+
+// we will learn chaining method 
+
+const num = myNumbs
+                    .map((num) => num * 10 ) //what is the num of the array? after multiplying the new arr is num now
+                    .map((num) => num + 1)
+                    .filter((num) => num < 25 )  // filer is true false kind of operation
+
+
+
+console.log(num);
+
+
+
+
+
+
+
+
+
+
+
 
 
 
