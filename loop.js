81072c0701eab9e902d5ddec7343160f9c7cd9e9
rohan-fa <rohanfarzana@gmail..com>
2023-10-14 13:08:08 +1100

    loops

--- loop.js
+++ loop.js
@@ -1,1 +1,25 @@
-//////////dwsafndinwusfiwnbf
+//for 
+
+
+for (let i = 0; i <= 2; i++) {
+    const element = i;
+    if(element == 1){
+        //console.log("fsd");
+    }
+    //console.log(element);
+}
+for (let i = 0; i < 2; i++) {
+   //console.log(`utterloop ${i}`);
+    for (let j = 0; j < 3; j++) {
+     //   console.log(`innerloop ${j} and innnerlop ${i}`);
+    }
+    
+}
+
+let arr = ["Rohan", "sfid","asdasd"]
+//console.log(arr.length);
+
+for (let i = 1; i < arr.length -1; i++) {
+    const element = arr[i];
+    //console.log(element);
+}
