c3027c95919ed17181a438595c40ac2fecc436bf
rohan-fa <rohanfarzana@gmail..com>
2023-10-15 21:49:13 +1100

    async

---
+++ asynchonous.js
@@ -0,0 +1,13 @@
+// in the jsEngineWebAPI how operations are executing lets explore that 
+
+//first => js code executes => creates a call stack => inside call stack there's global execution context creates => and ony by one function gets loaded. 
+
+// registered callback stores all the events orders
+
+
+//task queue: normal queue comes first goes first, 
+
+// fetchAPI => fetch api also works on task queue, it expand the task queue, maybe second task queue which in higher priority, inside fetch there's a promise concept comes. that means please
+// promise me you will do that work after that work, or if that a success tell me or not success also tell me that
+
+
